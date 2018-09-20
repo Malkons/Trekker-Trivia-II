87f43581a8863ef8e41fb86ad0a7a0ece7751094
Malkons <marc.alkons@gmail.com>
2018-09-20 16:42:36 -0700

    added css, and js logic

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# TriviaGame-v.2
-More advanced version of the TriviaGame
+# Trekker Trivia II
+
+## Overview
+More advanced version of the  Orignal TriviaGame.  This version the questions, choices and answers are stored as an array of objects. The app then loops through the array and uses jquery to format the html.
+
+## Directory Layout
+````
+TriviaGame-v.2
+    - assets
+        - css
+        - images
+        - javascript
+    - index.html
+
+````
+
+## Setup
+
+1. Deployed here: [https://malkons.github.io/Trekker-Trivia-II/](https://malkons.github.io/Trekker-Trivia-II/)
---
+++ assets/javascript/app.js
@@ -0,0 +1,178 @@
+$(document).ready(function () {
+    // Array of objects containing a question, answer array and correct answer
+    var triviaQuestions = [{
+        question: "Who was the Captain of the USS-Enterprise 1701-D?",
+        answerList: ["a. James Kirk", "b. Jean Luc Picard", "c. Rachel Garret", "d. Christopher Pike"],
+        answer: 1
+    }, {
+        question: "Today is a good day to die! A popular saying by the _________ ?",
+        answerList: ["a. Klingons", "b. Humans", "c. Romulans", "d. Vulcans"],
+        answer: 0
+    }, {
+        question: "Live Long and _________.",
+        answerList: ["a. Party", "b. Buy Property", "c. Prosper", "d. Peacfull"],
+        answer: 2
+    }, {
+        question: "Spock is half Vulcan, half ________ ?",
+        answerList: ["a. Human", "b. Klingon", "c. Romulan", "d. Andorian"],
+        answer: 0
+    }, {
+        question: "Terok Nor is the Cardassian name for this Space Station",
+        answerList: ["a. Starbase 47", "b. Deep Space Station K-7", "c. Jupiter Station", "d. Deep Space Nine"],
+        answer: 3
+    }, {
+        question: "First Contact was made with this alien species",
+        answerList: ["a. Romulan", "b. Klingon", "c. Ferengi", "d. Vulcans"],
+        answer: 3
+    }, {
+        question: "Starfleet Headquarters are located in this city",
+        answerList: ["a. San Francisco", "b. Paris", "c. Munich", "d. London"],
+        answer: 0
+    }, {
+        question: "How many Star Trek movies have there been all together?",
+        answerList: ["a. 10", "b. 15", "c. 13", "d. 8"],
+        answer: 2
+    }];
+
+    // Declare global variables
+    var correctAnswer;
+    var incorrectAnswer;
+    var answered;
+    var unanswered;
+    var time;
+    var seconds;
+    var playerChoice;
+    var currentQuestion;
+    var messages = {
+        correct: "That's Correct!",
+        incorrect: "Sorry, Incorrect",
+        outOfTime: "No Time Left",
+        complete: "Computing your results",
+    }
+
+    // Hides the borders of the current, answer and score div
+    $(".current").hide();
+    $(".answer").hide();
+    $(".score").hide();
+    // Start Button
+    $("#startBtn").on("click", function () {
+        $(this).hide();
+        newGame();
+    });
+
+    // Restart Button
+    $("#startOverBtn").on("click", function () {
+        $(this).hide();
+        newGame();
+    });
+
+    // Sets up a new game
+    function newGame() {
+        $(".current").show();
+        $("#finalMessage").empty();
+        $("#correctAnswers").empty();
+        $("#incorrectAnswers").empty();
+        $("#unanswered").empty();
+        currentQuestion = 0;
+        correctAnswer = 0;
+        incorrectAnswer = 0;
+        unanswered = 0;
+        newQuestion();
+    }
+
+    // Sets up a new question
+    function newQuestion() {
+        $(".answer").hide();
+        $(".score").hide();
+        $("#message").empty();
+        $("#correctedAnswer").empty();
+        answered = true;
+        // Displays current question number out of total question number "
+        $("#currentQuestion").html("Question #" + (currentQuestion + 1) + "/" + triviaQuestions.length);
+        // Displays question
+        $(".question").html("<h2>" + triviaQuestions[currentQuestion].question + "</h2>");
+        // Loop through answer array, creates a div for each answer and assigns attr of data index and a class of thischoice
+        for (var i = 0; i < 4; i++) {
+            var choices = $("<div>");
+            choices.text(triviaQuestions[currentQuestion].answerList[i]);
+            choices.attr({
+                "data-index": i
+            });
+            choices.addClass("thisChoice");
+            $(".answerList").append(choices);
+        }
+        countdown();
+        // Gets player answer
+        $(".thisChoice").on("click", function () {
+            playerChoice = $(this).data("index");
+            clearInterval(time);
+            answerPage();
+        });
+    }
+
+    // Countdown Timer
+    function countdown() {
+        seconds = 10;
+        answered = true;
+        $("#timeLeft").html("<h3>Time Remaining: " + seconds + "</h3>");
+        time = setInterval(showCountdown, 1000);
+    }
+    function showCountdown() {
+        seconds--;
+        $("#timeLeft").html("<h3>Time Remaining: " + seconds + "</h3>");
+        if (seconds === 0) {
+            clearInterval(time);
+            answered = false;
+            answerPage();
+        }
+    }
+
+    // Check Answer Function
+    function answerPage() {
+        $(".answer").show();
+        $("#currentQuestion").empty();
+        $(".thisChoice").empty();
+        $(".question").empty();
+
+        var rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
+        var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+
+        if ((playerChoice == rightAnswerIndex) && (answered == true)) {
+            correctAnswer++;
+            $("#message").html(messages.correct);
+        } else if ((playerChoice != rightAnswerIndex) && (answered == true)) {
+            incorrectAnswer++;
+            $("#message").html(messages.incorrect);
+            $("#correctedAnswer").html("The correct answer was: " + rightAnswerText);
+        } else {
+            unanswered++;
+            $("#message").html(messages.endTime);
+            $("#correctedAnswer").html("The correct answer was: " + rightAnswerText);
+            answered = true;
+        }
+
+        if (currentQuestion == (triviaQuestions.length - 1)) {
+            setTimeout(scoreboard, 2500)
+        } else {
+            currentQuestion++;
+            setTimeout(newQuestion, 3000);
+        }
+    }
+
+    // Results Function
+    function scoreboard() {
+        $(".score").show();
+        $(".current").hide();
+        $(".answer").hide();
+        $("#timeLeft").empty();
+        $("#message").empty();
+        $("#correctedAnswer").empty();
+       
+        $("#finalMessage").html(messages.finished);
+        $("#correctAnswers").html("Correct Answers: " + correctAnswer);
+        $("#incorrectAnswers").html("Incorrect Answers: " + incorrectAnswer);
+        $("#unanswered").html("Unanswered: " + unanswered);
+        $("#startOverBtn").addClass('reset');
+        $("#startOverBtn").show();
+    }
+});
